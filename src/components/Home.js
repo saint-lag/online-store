f745db21fde5dd0a7455f8cb0d3d7fdd07718cf2
HenriqueNasciment0 <hsncorretor@gmail.com>
2022-03-10 20:18:07 -0300

    requisitos 1 a 4

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.showCategories();
+  }
+
+  showCategories = async () => {
+    const category = await getCategories();
+    this.setState({ categories: category });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <main>
         <p data-testid="home-initial-message">
@@ -11,6 +29,13 @@ class Home extends React.Component {
         <Link to="/shopping-cart" data-testid="shopping-cart-button">
           <button type="button">Carrinho</button>
         </Link>
+        <section>
+          {categories.map((elem) => (
+            <button type="submit" key={ elem.id } data-testid="category">
+              { elem.name }
+            </button>
+          ))}
+        </section>
       </main>
     );
   }
