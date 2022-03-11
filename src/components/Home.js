5fbb317b74c28fa96731a2741764bab4ac450023
Gabriel Maia <maiasena.gabriel@gmail.com>
2022-03-10 22:29:00 -0300

    Home recebe Categorias

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
-// import Categories from './Categories';
 import './styles/home.css';
 
 class Home extends React.Component {
   render() {
     return (
       <main>
-        {/* <Categories /> */}
+        <Categories /> 
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
