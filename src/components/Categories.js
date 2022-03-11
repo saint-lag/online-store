c32be867dcbd7d565d675495ec62b439944840f0
saint-lag <maiasena.gabriel@gmail.com>
2022-03-10 21:39:50 -0300

    [Testado Localmente][Lint Testado Localmente]

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -12,33 +12,34 @@ class Categories extends React.Component {
 
   async componentDidMount() {
     this.setState({
-      categories: await getCategories().then((categories) => (categories)),
+      categories: await getCategories()
+        .then((categories) => categories
+          .map((category) => category.name)),
     });
   }
 
   createCategories() {
     const { categories } = this.state;
-    categories.map((category) => category.name).forEach((categoryName) => {
-      const categoryElDiv = document.createElement('div');
-      const categoryElBtn = document.createElement('button');
-      const categoryElLabel = document.createElement('label');
+    const mainNavChildren = [];
+    categories.forEach((categoryName) => {
+      const categoryElBtn = React.createElement('button',
+        { type: 'button', id: `${categoryName}Btn` });
+      const categoryElLabel = React.createElement('label',
+        { htmlFor: `${categoryName}Btn`, 'data-testid': 'category' }, categoryName);
+      const categoryElDiv = React.createElement('div', {},
+        categoryElBtn, categoryElLabel);
 
-      categoryElBtn.outerHTML = `<button id=${categoryName}Btn type="radio" >`;
-      categoryElLabel.outerHTML = `<label htmlFor=${categoryName}
-      Btn data-testid="category" ></label>`;
-      categoryElLabel.innerText = categoryName;
-
-      document.categoryElDiv.appendChild(categoryElBtn);
-      document.categoryElDiv.appendChild(categoryElLabel);
-      document.getElementById('main-nav').appendChild(categoryElDiv);
+      mainNavChildren.push(categoryElDiv);
     });
+    const mainNav = React.createElement('nav', { id: 'main-nav' }, mainNavChildren);
+    return mainNav;
   }
 
   render() {
     const { categories } = this.state;
     const { createCategories } = this;
     return (
-      <nav id="main-nav">{categories && createCategories}</nav>
+      categories && createCategories()
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Categories from './Categories';
 
 class Home extends React.Component {
   render() {
     return (
       <main>
+        <Categories />
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
