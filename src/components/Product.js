cb529883cc151497c16c0c1199af24da440e11e7
lucas_almeida <lucas.almdiniz@gmail.com>
2022-03-11 20:08:00 -0300

    Requisito 7 - criamos a pagina de informaçao do produto selecionado

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Product extends React.Component {
   render() {
-    const { title, image, price } = this.props;
+    const { title, image, price, id } = this.props;
     return (
-      <div data-testid="product">
-        <h3>{title}</h3>
-        <img src={ image } alt={ title } />
-        <p>{price}</p>
-      </div>
+      <Link to={ `/product/${id}` } data-testid="product-detail-link">
+        <div data-testid="product">
+          <h3>{title}</h3>
+          <img src={ image } alt={ title } />
+          <p>{price}</p>
+        </div>
+      </Link>
     );
   }
 }
@@ -18,6 +21,7 @@ Product.propTypes = {
   title: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Product;
