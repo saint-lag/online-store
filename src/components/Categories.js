78309a34163b3854cbdd8ef9edf21f258ce3626a
saint-lag <maiasena.gabriel@gmail.com>
2022-03-11 14:07:54 -0300

    [Refatoração][Componetização][Requirement 4][Testado Localmente][Lint] Adição de Labels e componetização de Categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -18,38 +18,51 @@ class Categories extends React.Component {
     });
   }
 
-  cssNamefyer(str) {
+  tagNamefyer(str) {
     const splitted = str.split(' ');
-    return splitted.map((word) => word
-      .toLowerCase()).join('-');
+    return splitted.map((word) => word.toLowerCase()).join('-');
   }
 
   createCategories() {
     const { categories } = this.state;
     const mainNavChildren = [];
     categories.forEach((categoryName) => {
-      const categoryElBtn = React.createElement('button',
-        { type: 'button', id: `${this.cssNamefyer(categoryName)}-btn` });
-      const categoryElLabel = React.createElement('label',
-        { htmlFor: `${this.cssNamefyer(categoryName)}-btn`,
-          'data-testid': 'category' },
-        categoryName);
-      const categoryElDiv = React.createElement('div',
-        { className: 'category-div', id: this.cssNamefyer(categoryName) },
-        categoryElBtn, categoryElLabel);
+      const tagName = this.tagNamefyer(categoryName);
+      const categoryElBtn = React.createElement('button', {
+        type: 'button',
+        id: `${tagName}-btn`,
+        key: `${tagName}-btn`,
+      });
+      const categoryElLabel = React.createElement(
+        'label',
+        {
+          key: `${tagName}-label`,
+          htmlFor: `${tagName}-btn`,
+          'data-testid': 'category',
+        },
+        categoryName,
+      );
+      const categoryElDiv = React.createElement(
+        'div',
+        { className: 'category-div', id: tagName, key: `${tagName}-div` },
+        categoryElBtn,
+        categoryElLabel,
+      );
 
       mainNavChildren.push(categoryElDiv);
     });
-    const mainNav = React.createElement('nav', { id: 'main-nav' }, mainNavChildren);
+    const mainNav = React.createElement(
+      'nav',
+      { id: 'main-nav' },
+      mainNavChildren,
+    );
     return mainNav;
   }
 
   render() {
     const { categories } = this.state;
     const { createCategories } = this;
-    return (
-      categories && createCategories()
-    );
+    return categories && createCategories();
   }
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './styles/home.css';
+import Categories from './Categories';
 
 class Home extends React.Component {
   render() {
     return (
       <main>
-        <Categories /> 
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Categories />
       </main>
     );
   }
